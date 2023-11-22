47ea1105de7ba323162235f1a217d3c5d79a6b5b
Whitney O'Meara <james.w.omeara@gmail.com>
2023-11-22 19:40:07 +0000

    Added producer confirms for metric client

--- src/main/java/datawave/microservice/querymetric/config/QueryMetricClientProperties.java
+++ src/main/java/datawave/microservice/querymetric/config/QueryMetricClientProperties.java
@@ -1,6 +1,11 @@
 package datawave.microservice.querymetric.config;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.validation.constraints.PositiveOrZero;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "datawave.query.metric.client")
 public class QueryMetricClientProperties {
@@ -10,6 +15,10 @@ public class QueryMetricClientProperties {
     private int port = 8443;
     private String updateMetricUrl = "/querymetric/v1/updateMetric";
     private String updateMetricsUrl = "/querymetric/v1/updateMetrics";
+    private boolean confirmAckEnabled = true;
+    private long confirmAckTimeoutMillis = 500L;
+    
+    Retry retry = new Retry();
     
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
@@ -57,5 +66,65 @@ public class QueryMetricClientProperties {
     
     public void setUpdateMetricsUrl(String updateMetricsUrl) {
         this.updateMetricsUrl = updateMetricsUrl;
+    }
+    
+    public boolean isConfirmAckEnabled() {
+        return confirmAckEnabled;
+    }
+    
+    public void setConfirmAckEnabled(boolean confirmAckEnabled) {
+        this.confirmAckEnabled = confirmAckEnabled;
+    }
+    
+    public long getConfirmAckTimeoutMillis() {
+        return confirmAckTimeoutMillis;
+    }
+    
+    public void setConfirmAckTimeoutMillis(long confirmAckTimeoutMillis) {
+        this.confirmAckTimeoutMillis = confirmAckTimeoutMillis;
+    }
+    
+    public Retry getRetry() {
+        return retry;
+    }
+    
+    public void setRetry(Retry retry) {
+        this.retry = retry;
+    }
+    
+    @Validated
+    public static class Retry {
+        @PositiveOrZero
+        private int maxAttempts = 10;
+        
+        @PositiveOrZero
+        private long failTimeoutMillis = TimeUnit.MINUTES.toMillis(5);
+        
+        @PositiveOrZero
+        private long backoffIntervalMillis = TimeUnit.SECONDS.toMillis(5);
+        
+        public int getMaxAttempts() {
+            return maxAttempts;
+        }
+        
+        public void setMaxAttempts(int maxAttempts) {
+            this.maxAttempts = maxAttempts;
+        }
+        
+        public long getFailTimeoutMillis() {
+            return failTimeoutMillis;
+        }
+        
+        public void setFailTimeoutMillis(long failTimeoutMillis) {
+            this.failTimeoutMillis = failTimeoutMillis;
+        }
+        
+        public long getBackoffIntervalMillis() {
+            return backoffIntervalMillis;
+        }
+        
+        public void setBackoffIntervalMillis(long backoffIntervalMillis) {
+            this.backoffIntervalMillis = backoffIntervalMillis;
+        }
     }
 }
